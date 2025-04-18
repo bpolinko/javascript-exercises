2880021e0490e6ebab78e3651da7accf4cb92eb7
bpolinko <b.polinko@gmail.com>
2025-04-18 15:37:38 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const punct = ["!", ".", ",", " "];
+  const forwordArray = word.split("");
+  const reverseArray = word.split("").reverse();
+  
+  const resultF = forwordArray.filter(removePunct);
+  const resultR = reverseArray.filter(removePunct);
 
+  const joinF = resultF.join("").toLowerCase();
+  const joinR = resultR.join("").toLowerCase();
+
+  function removePunct(letter) {
+    return !punct.includes(letter);
+  }
+  
+  if (joinF == joinR) {
+    return true;
+  } else {
+    return false;
+  }
+  
+  
 };
 
 // Do not edit below this line
