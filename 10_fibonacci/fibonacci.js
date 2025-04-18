f7aad6bcd66f7574297d8d211f3019fcbcd523da
bpolinko <b.polinko@gmail.com>
2025-04-18 16:13:18 -0400

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const num = Number(input);
+    if (num < 0) {return "OOPS";}
+    else {
+    const sequence = [0, 1];
+      for (let i = 2; i <= num; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+        }
+        return sequence[num];
+      }
+    
 };
 
 // Do not edit below this line
